4f7989640eba5242abf918d95d11d855b7c57d70
Oscar Van Slijpe <30955488+OscarVsp@users.noreply.github.com>
2022-04-29 14:41:42 +0200

    Update MMM-Relay-Scheduler.js

--- MMM-Relay-Scheduler.js
+++ MMM-Relay-Scheduler.js
@@ -10,23 +10,24 @@
 Module.register('MMM-Relay-Scheduler',{
 	defaults: {
 		relayPin: 14,
-		daysOff: [5,6],
-		startTime: 8*60,
-		endTime: 18*60,
-		refreshInterval: 1000*10
+		daysOff: [5,6],		//Starting at 0 for Monday, so [5,6] is Saturday and Sunday
+		startTime: 8*60,		//8:00
+		endTime: 18*60,			//18:00
+		refreshInterval: 1000*60	//Check every minute
 	},
 
 	start: function () {
 		Log.info("Starting module: " + this.name);
 		this.sendSocketNotification("MMM-Relay-Scheduler-Setup",{config: this.config,identifier: this.identifier})
 		var self = this;
-		self.checkState();
+		// Wait a bit before first state check so that the node_helper get initialized.
+		setTimeout(self.checkState(),1000);
 		// Schedule updates
 		setInterval(function () {
 		  self.checkState();
 		}, this.config.refreshInterval);
 	  },
-
+	//Ask to the node_helper to check the state
 	checkState: function () {
 		Log.info("MMM-Relay-Scheduler: start check state");
 		this.sendSocketNotification("MMM-Relay-Scheduler-Check-State", {
