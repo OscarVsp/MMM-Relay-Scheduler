0ce9c5ca3e75e8b72650ea9736dfe8f6f2787358
Oscar Van Slijpe <30955488+OscarVsp@users.noreply.github.com>
2022-04-29 14:41:55 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -8,49 +8,60 @@
 const NodeHelper = require('node_helper');
 const Gpio = require('onoff').Gpio;
 
+
 module.exports = NodeHelper.create({
     start: function () {
         this.started = false;
+        this.relayState = 1;
     },
   
     socketNotificationReceived: function (notification, payload) {
         var self = this;
 
+        //Initialize the relay state and pinout
         if (notification == "MMM-Relay-Scheduler-Setup"){
-            this.relayState = 0;
+            this.relayState = 1;
             this.relay = new Gpio(payload.config.relayPin, 'out');
             this.relay.writeSync(this.relayState);
             this.started = true
         }
-        
+        //Check new the state
         if (notification === "MMM-Relay-Scheduler-Check-State") {
-            const d = new Date();
-            let dayOfTheWeek = (d.getDay() - 1)%7;       //To get Monday as day number 0 instead of Sunday             
+            //Check that the relay has been initialize, to avoid getting error
+            if (this.started){
+                const d = new Date();
+                let dayOfTheWeek = (d.getDay() - 1)%7;       //To get Monday as day number 0 instead of Sunday             
 
-            if (this.relayState == 1) {
-                //Check if we are in day off
-                if (payload.config.daysOff.includes(dayOfTheWeek)){
-                    console.log("MMM-Relay-Scheduler: relay turning off because this is a day off.");
-                    this.relayState = 0;
-                } else {
-                    //Check the time
-                    let time = d.getHours() * 60 + d.getMinutes();
-                    if (time < payload.config.startTime || time >= payload.config.endTime) {
-                        console.log("MMM-Relay-Scheduler: relay turning off because this is a time off.");
+                //Case current state is on
+                if (this.relayState == 1) {
+                    //Turn off if we are in day off
+                    if (payload.config.daysOff.includes(dayOfTheWeek)){
+                        console.log("MMM-Relay-Scheduler: relay turning off because this is a day off.");
                         this.relayState = 0;
+                        this.relay.writeSync(this.relayState);
+                    } else {
+                        //Turn off if we are not in working hours
+                        let time = d.getHours() * 60 + d.getMinutes();
+                        if (time < payload.config.startTime || time >= payload.config.endTime) {
+                            console.log("MMM-Relay-Scheduler: relay turning off because this is a time off.");
+                            this.relayState = 0;
+                            this.relay.writeSync(this.relayState);
+                        }
                     }
-                }
-            } else {
-                if (!payload.config.daysOff.includes(dayOfTheWeek)){
-                    let time = d.getHours() * 60 + d.getMinutes();
-                    if (time >= payload.config.startTime && time < payload.config.endTime) {
-                        console.log("MMM-Relay-Scheduler: relay turning on");
-                        this.relayState = 1;
-                        
+                //Case current state if off
+                } else {
+                    //First check if we are in day on before getting the date
+                    if (!payload.config.daysOff.includes(dayOfTheWeek)){
+                        let time = d.getHours() * 60 + d.getMinutes();
+                        //Then turn on if we are also in working hours
+                        if (time >= payload.config.startTime && time < payload.config.endTime) {
+                            console.log("MMM-Relay-Scheduler: relay turning on");
+                            this.relayState = 1;
+                            this.relay.writeSync(this.relayState);
+                        }
                     }
                 }
             }
-            this.relay.writeSync(this.relayState);
         }
     }
   });
